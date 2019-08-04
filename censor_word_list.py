3955d758b27d202f2b158e60a862fa8dd78a131c
CuriousPato <patryck.shepard@gmail.com>
2019-08-04 12:48:41 -0500

    finished function to censor learning algorithm.
    2nd problem more challenging than I thought!

---
+++ censor_word_list.py
@@ -0,0 +1,13 @@
+# These are the emails you will be censoring. The open() function is opening the text file that the emails are contained in and the .read() method is allowing us to save their contexts to the following variables:
+email_one = open("email_one.txt", "r").read()
+email_two = open("email_two.txt", "r").read()
+email_three = open("email_three.txt", "r").read()
+email_four = open("email_four.txt", "r").read()
+
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+
+def censor_terms(email, terms_list):
+    content_clipped = email.split(terms_list)
+    return(content_clipped)
+
+print(censor_terms(email_two,proprietary_terms))
